90d87745efd3106a74d7dc7d5cf1879153d8d366
Korto <kortostocks@gmail.com>
2026-02-04 10:27:20 -0500

    while con numeros

--- codigo.js
+++ codigo.js
@@ -19,7 +19,32 @@ let procesador = pc1["procesador"]
 let ram = pc1["ram"]
 let espacio = pc1["espacio"]
 
-document.getElementById("ArraysASO").textContent = `el nombre de mi PC es: ${nombre} <br> 
-    el nombre de mi PC es: ${procesador} <br> 
-    el nombre de mi PC es: ${ram} <br> 
-    el nombre de mi PC es: ${espacio} <br>`
+document.getElementById("ArraysASO").textContent = 
+    `el nombre de mi PC es: ${nombre} 
+    el nombre de mi PC es: ${procesador} 
+    el nombre de mi PC es: ${ram} 
+    el nombre de mi PC es: ${espacio} `;
+
+
+    let numero = 0;
+    if (numero < 10 ){
+        numero++;
+        numero++;
+        numero++;//aumenta uno
+        document.getElementById("Bucles").textContent = numero;
+    } while (numero < 10 ){
+        numero++;//lo suma hasta que ya no sea mas grande que diez porqu seria false
+        document.getElementById("While").textContent = numero
+    }
+
+let numero1 = 0;
+
+while (numero1 < 1000) {
+    numero1++;
+    document.getElementById("while2").textContent = numero1;
+
+    if (numero1 === 100) {
+        break;
+    }
+}
+
