c6cb5b310f91f99cfa69e8f7d7c88910e020383e
Korto <kortostocks@gmail.com>
2026-02-10 14:00:15 -0500

    cominzo del tercer

---
+++ index.js
@@ -0,0 +1,10 @@
+class animal {
+    contructor(especie,edad,color){
+        this.especie =especie;
+        this.edad = edad;
+        this.color = color;
+    }
+}
+
+let perro = new animal("perro","3","rojo")
+console.log(perro)
