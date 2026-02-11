cb0f7d5b935238777e141ccc29b609aa63bc704b
Korto <kortostocks@gmail.com>
2026-02-11 14:03:35 -0500

    get y set

--- index.js
+++ index.js
@@ -14,13 +14,41 @@ class Perro extends Animal {
     
     this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años, soy de color ${this.color} y mi raza es ${this.raza}`;
     }
-    ladrar(){
+    ladrar() {
         alert("gua guau")
     }
 }
 
+class Gato extends Animal {
+    constructor(especie, edad, color, patas){
+        super(especie, edad, color);
+        this._patas = patas; // "_" indica que es interno
+
+        this.actualizarInformacion();
+    }  
+
+    set modificarPatas(nuevoNumero) {
+        this._patas = nuevoNumero;
+        this.actualizarInformacion();
+    }
+
+    get modificarPatas(){
+        return this._patas;
+    }
+
+    actualizarInformacion() {
+        this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años, soy de color ${this.color} y tengo ${this._patas} patas`;
+    }
+
+    static mew() {
+        alert("miaw");
+    }
+}
+
+
+
 let perro = new Perro("perro", 3, "rojo", "doberman");
-let gato = new Animal("gato", 5, "negro");
+let gato = new Gato("gato", 5, "negro",4);
 let pajaro = new Animal("pajaro", 4, "negro");
 
 let listaAnimales = [perro, gato, pajaro];
@@ -33,5 +61,10 @@ listaAnimales.forEach(animal => {
     contenedor.innerHTML += `<p>${animal.informacion}</p>`;
 });
 
+
+gato.modificarPatas = 3
 perro.ladrar();
-gato.ladrar();//sale que no es funcion ya es una herencia a perro y no a gato
+Gato.mew()//static no va por el objeto sino por la raiz 
+
+
+
