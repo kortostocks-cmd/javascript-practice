11fad1ef632254ebbf4af632e5218e80fad91d88
Korto <kortostocks@gmail.com>
2026-02-11 16:26:40 -0500

    capitulo 3 completo de cofla

--- index.js
+++ index.js
@@ -1,70 +1,125 @@
-class Animal {
-    constructor(especie, edad, color) {
-        this.especie = especie;
-        this.edad = edad;
-        this.color = color;
-        this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}`;
-    }
-}
+// class Animal {
+//     constructor(especie, edad, color) {
+//         this.especie = especie;
+//         this.edad = edad;
+//         this.color = color;
+//         this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años y soy de color ${this.color}`;
+//     }
+// }
 
-class Perro extends Animal {
-    constructor(especie, edad, color, raza){
-        super(especie, edad, color);
-        this.raza = raza;
+// class Perro extends Animal {
+//     constructor(especie, edad, color, raza){
+//         super(especie, edad, color);
+//         this.raza = raza;
     
-    this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años, soy de color ${this.color} y mi raza es ${this.raza}`;
+//     this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años, soy de color ${this.color} y mi raza es ${this.raza}`;
+//     }
+//     ladrar() {
+//         alert("gua guau")
+//     }
+// }
+
+// class Gato extends Animal {
+//     constructor(especie, edad, color, patas){
+//         super(especie, edad, color);
+//         this._patas = patas; // "_" indica que es interno
+
+//         this.actualizarInformacion();
+//     }  
+
+//     set modificarPatas(nuevoNumero) {
+//         this._patas = nuevoNumero;
+//         this.actualizarInformacion();
+//     }
+
+//     get modificarPatas(){
+//         return this._patas;
+//     }
+
+//     actualizarInformacion() {
+//         this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años, soy de color ${this.color} y tengo ${this._patas} patas`;
+//     }
+
+//     static mew() {
+//         alert("miaw");
+//     }
+// }
+
+
+
+// let perro = new Perro("perro", 3, "rojo", "doberman");
+// let gato = new Gato("gato", 5, "negro");
+// let pajaro = new Animal("pajaro", 4, "negro");
+
+// let listaAnimales = [perro, gato, pajaro];
+
+// let contenedor = document.getElementById("Objeto");
+
+// contenedor.innerHTML = ""; 
+
+// listaAnimales.forEach(animal => {
+//     contenedor.innerHTML += `<p>${animal.informacion}</p>`;
+// });
+
+
+// gato.modificarPatas = 3
+// perro.ladrar();
+//Gato.mew()//static no va por el objeto sino por la raiz 
+
+
+//cofla 3 cap 3
+//cofla 1 de 3 celulares
+class Celular {
+    constructor(nombre,color,tamaño,peso,rdp,rdc,ram) {
+        this.nombre = nombre;
+        this.color = color;
+        this.peso = peso;
+        this.tamaño = tamaño;
+        this.resolucionDePantalla = rdp;
+        this.resolucionDeCamara = rdc;
+        this.memoriaRam = ram;
+        this.encendido = false;
     }
-    ladrar() {
-        alert("gua guau")
+
+    mobileInfo(){
+        return `
+        <div class="celular">
+            <h3>${this.nombre}</h3>
+            Color: <b>${this.color}</b><br>
+            Peso: <b>${this.peso}</b><br>
+            Tamaño: <b>${this.tamaño}</b><br>
+            Resolución de Cámara: <b>${this.resolucionDeCamara}</b><br>
+            Resolución de Pantalla: <b>${this.resolucionDePantalla}</b><br>
+            Memoria RAM: <b>${this.memoriaRam}</b><br>
+        </div>
+        `;
     }
 }
 
-class Gato extends Animal {
-    constructor(especie, edad, color, patas){
-        super(especie, edad, color);
-        this._patas = patas; // "_" indica que es interno
-
-        this.actualizarInformacion();
-    }  
-
-    set modificarPatas(nuevoNumero) {
-        this._patas = nuevoNumero;
-        this.actualizarInformacion();
+class CelularAltaGama {
+    constructor(nombre,color,tamaño,peso,rdp,rdc,ram,rdce){
+        super(nombre,color,tamaño,peso,rdp,rdc,ram)
+        this.resolucionDeCamaraExtra = rdce;
     }
-
-    get modificarPatas(){
-        return this._patas;
+    grabarVideoLento(){
+        alert("estas grabando en camara lenta");
     }
-
-    actualizarInformacion() {
-        this.informacion = `Soy un ${this.especie}, tengo ${this.edad} años, soy de color ${this.color} y tengo ${this._patas} patas`;
-    }
-
-    static mew() {
-        alert("miaw");
+    reconocimientoFacial(){
+        alert("vamos a iniciar un reconocimiento facial")
     }
 }
 
+// Creamos un array de celulares
+const celulares = [
+    new Celular("iPhone","rojo","grande","150g","5'","Full HD","3GB"),
+    new Celular("Samsung","azul","pequeño","145g","5.4'","1920x720","2GB"),
+    new Celular("Xiaomi","negro","mediano","130g","5.9'","1820 HD","4GB")
+];
 
+const contenedor = document.getElementById("cualidades");
 
-let perro = new Perro("perro", 3, "rojo", "doberman");
-let gato = new Gato("gato", 5, "negro");
-let pajaro = new Animal("pajaro", 4, "negro");
-
-let listaAnimales = [perro, gato, pajaro];
-
-let contenedor = document.getElementById("Objeto");
-
-contenedor.innerHTML = ""; 
-
-listaAnimales.forEach(animal => {
-    contenedor.innerHTML += `<p>${animal.informacion}</p>`;
+// Recorremos el array
+celulares.forEach(celular => {
+    contenedor.innerHTML += celular.mobileInfo();
 });
-
-
-gato.modificarPatas = 3
-perro.ladrar();
-Gato.mew()//static no va por el objeto sino por la raiz 
-
-
 
